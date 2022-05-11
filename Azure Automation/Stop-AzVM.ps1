39b1d98563dac6dbd23d2df3f2d20ff4aefec2f7
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2022-05-11 17:31:01 +1200

    Update Stop-AzVM.ps1

--- Azure Automation/Stop-AzVM.ps1
+++ Azure Automation/Stop-AzVM.ps1
@@ -1,4 +1,4 @@
-#requires -Version 3.0 -Modules Az.Accounts, Az.Resources
+#requires -Version 3.0 -Modules Az.Accounts, Az.AlertsManagement
 <#
     .SYNOPSIS
     PowerShell Azure Automation Runbook for Stopping Virtual Machines, that have been Shutdown within the Windows Operating System (Stopped and not Deallocated). 
@@ -11,6 +11,7 @@ param (
     [Parameter(Mandatory = $true, HelpMessage = 'Data from the WebHook/Azure Alert')][Object]$WebhookData
 )
 
+Import-Module Az.AlertsManagement
 $ErrorActionPreference = 'stop'
 
 # Get the data object from WebhookData
@@ -23,7 +24,9 @@ $Essentials = [object] ($Schema.data).essentials
 Write-Output -InputObject $Essentials 
 
 # Get the first target only as this script doesn't handle multiple and and export variables for the resource.
+$alertIdArray = (($Essentials.alertId)).Split('/')
 $alertTargetIdArray = (($Essentials.alertTargetIds)[0]).Split('/')
+$alertid =  ($alertIdArray)[6]
 $SubId = ($alertTargetIdArray)[2]
 $ResourceGroupName = ($alertTargetIdArray)[4]
 $ResourceType = ($alertTargetIdArray)[6] + '/' + ($alertTargetIdArray)[7]
@@ -56,16 +59,23 @@ if (($status -eq 'Activated') -or ($status -eq 'Fired')) {
         Write-Output   -InputObject $AzureContext 
         #Checks Azure VM status
         $VMStatus = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $ResourceName -Status
+
         Write-Output  -InputObject $VMStatus
         If ($VMStatus.Statuses[1].Code -eq 'PowerState/stopped') {
-            Write-Output  -InputObject "Stopping the VM, it was Shutdown without being Deallocated - $ResourceName - in resource group - $ResourceGroupName -" -Verbose
-            Stop-AzVM -Name $ResourceName -ResourceGroupName $ResourceGroupName -DefaultProfile $AzureContext -Force
+            Write-Output  -InputObject "Stopping the VM, it was Shutdown without being Deallocated - $ResourceName - in resource group - $ResourceGroupName" -Verbose
+            Stop-AzVM -Name $ResourceName -ResourceGroupName $ResourceGroupName -DefaultProfile $AzureContext -Force -Verbose
       
             #Check VM Status after deallocation
-            $VMStatus = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $ResourceName -Status
+            $VMStatus = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $ResourceName -Status -Verbose
+            
+            Write-Output  -InputObject $VMStatus
+
             If ($VMStatus.Statuses[1].Code -eq 'PowerState/deallocated') {
                 #Closes Alert
-                Update-AzAlertState -AlertId "$Essentials.originAlertid" -State "Closed"
+                Write-Output  -InputObject $VMStatus.Statuses[1].Code
+                Write-Output  -InputObject $alertid 
+                Get-AzAlert -AlertId $alertid  -verbose -DefaultProfile $AzureContext
+                Get-AzAlert -AlertId $alertid  -verbose -DefaultProfile $AzureContext | Update-AzAlertState -State 'Closed' -Verbose -DefaultProfile $AzureContext
             }
         }
              
